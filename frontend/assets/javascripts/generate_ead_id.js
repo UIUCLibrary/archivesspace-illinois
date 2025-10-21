be397a6e063087cee4ff7d6adc0c75816fe1ee2c
Leonel Ramirez <50677216+leonelramirez@users.noreply.github.com>
2025-10-20 19:45:58 -0500

    feat: updated sort title field

--- frontend/assets/javascripts/generate_ead_id.js
+++ frontend/assets/javascripts/generate_ead_id.js
@@ -1,7 +1,7 @@
-// Generates the EAD ID field value based on other identifier fields in the resource form
-// Joins non-empty identifier fields with a dot (.) and removes whitespaces
-// Updates the ead_id field in real-time as the user types
 $(function () {
+  // Generates the EAD ID field value based on other identifier fields in the resource form
+  // Joins non-empty identifier fields with a dot (.) and removes whitespaces
+  // Updates the ead_id field in real-time as the user types
   var initEadIdField = function (scope) {
     scope = scope || $(document.body);
     $eadIdInput = $('input[name="resource[ead_id]"]', scope)
@@ -31,8 +31,33 @@ $(function () {
       );
     }
   };
+
+  // Moves the finding_aid_filing_title field next to the title field
+  // and synchronizes its value with the title field in real-time
+  var initSortTitleField = function (scope) {
+    $filingTitle = $('textarea[name="resource[finding_aid_filing_title]"]', scope);
+    $title = $('textarea[name="resource[title]"]', scope);
+
+    $title.parents('.form-group:first').after($filingTitle.parents('.form-group:first'));
+
+    // set initial value of finding_aid_filing_title if empty
+    if ($filingTitle.val().length === 0) {
+      $filingTitle.val($title.val());
+    }
+
+    $title.on(
+      'keyup',
+      function (event) {
+        // copy value to finding_aid_filing_title field
+        $filingTitle.val($(event.target).val());
+      }
+    )
+  }
+
   $(document).bind('loadedrecordform.aspace', function (event, $container) {
     initEadIdField($container);
+    initSortTitleField($container);
   });
   initEadIdField();
+  initSortTitleField();
 });
